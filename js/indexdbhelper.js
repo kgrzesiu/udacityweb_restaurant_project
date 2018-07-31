b67c557b83fa549899a9177f0e13a5f736b75f6e
Greg Kazm <kgrzesiu@gmail.com>
2018-07-31 20:05:57 +0200

    Initial database helper working test

--- js/indexdbhelper.js
+++ js/indexdbhelper.js
@@ -1,15 +1,66 @@
-console.log('Imporetd Insdgfaksdjfasdkjfasdkjfnasdkjfasdkfnj');
+console.log('Imporetd IndexDBHelper');
 
 class IndexDBHelper { 
    /**
-   * Open database
+   * Open database, and returns the promise
    */
   static openDatabase() {
-    console.log('XXXXXXXXXXXXXXXX','Database helper is working!',IndexDBHelper.DB_NAME);
+    return idb.open(IndexDBHelper.DB_NAME, 3, function(upgradeDb){
+        console.log('Opening database');
+        switch(upgradeDb.oldVersion){
+            case 0:
+                upgradeDb.createObjectStore(IndexDBHelper.RESTAURANTS, { keyPath:'id'});
+            case 1:
+                var restStore = upgradeDb.transaction.objectStore(RESTAURANTS);
+                restStore.createIndex(IndexDBHelper.NEIGHBORHOOD_INDEX,IndexDBHelper.NEIGHBORHOOD_PROP);
+                restStore.createIndex(IndexDBHelper.CUISINE_INDEX,IndexDBHelper.CUISINE_PROP);
+        }
+    });
+  }
+  
+  /**
+   * Get's all the restaurants
+   */
+  static getAllRestaurants() {
+    var dbPromise = IndexDBHelper.openDatabase();
+    return dbPromise.then(function(db){
+        var tx = db.transaction(IndexDBHelper.RESTAURANTS);
+        var restaurantsStore = tx.objectStore(IndexDBHelper.RESTAURANTS);
+        return restaurantsStore.getAll();
+    });
+  }
+
+  static saveRestaurant(restaurant) {
+    var dbPromise = IndexDBHelper.openDatabase();
+    return dbPromise.then(function(db){
+        var tx = db.transaction(IndexDBHelper.RESTAURANTS,'readwrite');
+        var restStore = tx.objectStore(IndexDBHelper.RESTAURANTS);
+        restStore.put(restaurant);
+    });
+  }
+
+  static saveRestaurantWithPromise(dbPromise, restaurant) {
+    return dbPromise.then(function(db){
+        return db.transaction(IndexDBHelper.RESTAURANTS,'readwrite')
+        .objectStore(IndexDBHelper.RESTAURANTS)
+        .put(restaurant);
+    });
+  }
+
+  static getRestaurantWithPromise(dbPromise, id) {
+    return dbPromise.then(function(db){
+        return db.transaction(IndexDBHelper.RESTAURANTS,'readonly')
+        .objectStore(IndexDBHelper.RESTAURANTS)
+        .get(id);
+    });
   }
 }
 
 IndexDBHelper.DB_NAME = 'stage2db';
 IndexDBHelper.RESTAURANTS = 'restaurants';
+
 IndexDBHelper.CUISINE_INDEX = 'cuisine';
+IndexDBHelper.CUISINE_PROP = 'cuisine_type';
+
 IndexDBHelper.NEIGHBORHOOD_INDEX = 'neighborhood';
+IndexDBHelper.NEIGHBORHOOD_PROP = 'neighborhood';
